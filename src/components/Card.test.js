f6d6f13e8c426e405c244be0946f5bde1396e2e6
Lajancia <50996139+Lajancia@users.noreply.github.com>
2024-01-30 00:13:24 +0900

    [fix] : 불필요한 코드 제거

--- src/components/Card.test.js
+++ src/components/Card.test.js
@@ -1,9 +1,8 @@
 import { fireEvent } from '@testing-library/dom';
-import Card from './Card.js'; // adjust the path to your Card module
+import Card from './Card.js'; 
 
 describe('페이지 이동 동작 확인', () => {
     test('URL 변경 확인 테스트', () => {
-        // Create a mock div as your app
         const app = document.createElement('div');
         app.id = 'app';
         document.body.appendChild(app);
